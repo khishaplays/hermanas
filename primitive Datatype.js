eb7608433fc59720822e203406ceeea711548377
Your Name <lakhishaloud028@gmail.com>
2026-06-17 14:36:52 +0300

    primitiveDatatypes

---
+++ primitive Datatype.js
@@ -0,0 +1,13 @@
+/*
+Numbers.
+floating point number:
+real nummbers
+positive
+negative numbers
+*/
+let number1=23;//real number
+console.log(number1);
+let number2=34.34;//floating number
+console.log(number2);
+let number3=-5;//negative number
+console.log(number3);
