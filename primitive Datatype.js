b9d63fe114cad0be6db87341510214abfcae17a5
Your Name <lakhishaloud028@gmail.com>
2026-06-17 15:26:33 +0300

    update changes

--- primitive Datatype.js
+++ primitive Datatype.js
@@ -11,3 +11,56 @@ let number2=34.34;//floating number
 console.log(number2);
 let number3=-5;//negative number
 console.log(number3);
+
+/*
+Strings-stores sequence of characters /text.
+Ways of creating strings:
+1. using single quotes
+2. using double quotes
+3. using backticks
+*/
+
+let str1='khisha types';//single quotes
+console.log(str1);
+let str2="khisha codes"; //double quotes
+console.log(str2);
+let str3=`khisha learns`; //backticks
+console.log(str3);
+
+/*reasons for different ways of creating strings:
+1. single quotes and double quotes are used to create simple strings.
+2. backticks are used to create complex strings.
+3. backticks allow us to use string interpolation.
+4. backticks allow us to create multi-line strings.
+5. backticks allow us to use special characters without escaping them.  
+ */                         
+//below is an example of how to use backticks without escaping special characters.
+let str4=`khisha's example`;
+console.log(str4);
+
+// alsofound an example of how string interpolation works in backticks.
+let name='khisha';
+let str5=`my name is ${name}`;
+console.log(str5);
+
+
+/*Next is-Boolean:represents true or false values.*/
+let khishaIsListening=true;
+console.log(khishaIsListening);
+let khishaIsSleeping=false;
+console.log(khishaIsSleeping);
+
+
+/*Null:represents the absence of any value.
+ It is a primitive data type that has only one value: null.
+  It is used to indicate that a variable has no value or that an object property does not exist.*/
+let emptyValue=null;
+console.log(emptyValue);
+
+/*Undefined:represents the absence of a value or the absence of a variable.
+ It is a primitive data type that has only one value: undefined.
+  It is used to indicate that a variable has not been assigned a value or that an object property does not exist.
+ Note: never explicitly assign undefined to a variable. 
+ It is better to use null to indicate the absence of a value.*/
+  
+
