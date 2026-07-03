b406548e267231ae1c3dff0fb4abcf1a24644c69
Your Name <lakhishaloud028@gmail.com>
2026-07-03 11:09:19 +0300

    friday

---
+++ junior-dev/recursive.js
@@ -0,0 +1,14 @@
+function salaryGrossPromptProcess(message = "Enter your gross salary:") {
+  let gross = Number(prompt(message));
+
+  if (!isNaN(gross)) {
+    console.log(`You entered ${gross}, exiting`);
+    return gross;
+  }
+
+  return salaryGrossPromptProcess(
+    "Invalid gross salary entered. Please enter a valid number for gross salary. Try again:",
+  );
+}
+
+salaryGrossPromptProcess();
