854293da22bd82ac1973abbd7c37e8e4a3b8395a
Your Name <lakhishaloud028@gmail.com>
2026-07-07 14:51:56 +0300

    sabasaba

--- taxcalculator.js/basic-engineer/forLoop.js
+++ taxcalculator.js/basic-engineer/forLoop.js
@@ -1,8 +1,25 @@
 /*for loop-meant for numbers
 combination of using while loop and for loop
 */
-let k = 0;
 
-for (let k = 0; k < 30; k = k + 1) {
-  console.log("k is", k);
+function mult() {
+  let num1 = Number(prompt("Enter first number (> 1):"));
+  let num2 = Number(prompt("Enter second number (> 1):"));
+
+  if (num1 <= 1 || num2 <= 1) {
+    console.log("ensure that the numbers are greater than 1");
+    return;
+  }
+
+  table(num1, num2);
 }
+
+function table(num1, num2) {
+  for (let i = 1; i <= num1; i++) {
+    for (let j = 1; j <= num2; j++) {
+      console.log(`${i} * ${j} = ${i * j}`);
+    }
+  }
+}
+
+mult();
