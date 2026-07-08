898af324df8377d059d57f73f9f14c5264fc3692
Your Name <lakhishaloud028@gmail.com>
2026-07-08 15:51:10 +0300

    2

--- basic-engineer/node-virus/bash.js
+++ basic-engineer/node-virus/bash.js
@@ -1,13 +1,20 @@
 const fs = require("fs");
-function testAppendFile() {
-  const filedata = "\nHey its khisha\n";
-  const cb = (error) => {
-    if (error) {
-      console.log("creating file failed ");
-      console.log(error);
-    }
-  };
-  fs.appendFile("test.txt", filedata, cb);
+
+function createFile() {
+  let currentDate = new Date();
+  let fileName = `file-${currentDate.getTime()}.txt`;
+  let fileData = `This is a test file created on ${currentDate.toLocaleString()}`;
+
+  let data = " ";
+  for (let i = 0; i < 10000; i++) {
+    data += `This is khisha$`;
+    fs.appendFile("currentdate.txt", fileData, (error) => {
+      if (error) {
+        console.log("Failed to append to file");
+        console.log(error);
+      }
+      console.log(`File ${fileName} created successfully!`);
+    });
+  }
 }
-testAppendFile();
-  function
+createFile();
