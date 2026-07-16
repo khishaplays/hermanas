6ff65aa788c259c7cec298eb6b7c4787a5e4e710
Your Name <lakhishaloud028@gmail.com>
2026-07-16 11:09:53 +0300

    postman

--- JS/git-hubproject/index.js
+++ JS/git-hubproject/index.js
@@ -10,3 +10,24 @@ fetch("https://api.github.com/users", requestOptions)
     console.log(result);
   })
   .catch((error) => console.error(error));
+
+  /*
+code without a promise.
+1. create a variable <any name> global variable
+2. console.log the value of the variable.
+3. create and call a function that modifies this variable
+4. console.log( ) the value of the variable
+*/
+
+let read = "This read";
+console.log("36:", read);       
+fetch("https://api.github.com/users", requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
+  })
+function testfunc() {
+  read = "updated read";
+}
+
+testfunc();
+console.log("41:", read);       
